e3cd6af4a6f6ab721678abdb06ab1e06e5972c44
Deyan Yanakiev <kliomba@abv.bg>
2015-10-25 12:07:23 +0200

    Create index.html for NoteWrangler app

--- NoteWrangler/server/expressConfig.js
+++ NoteWrangler/server/expressConfig.js
@@ -14,4 +14,4 @@ module.exports = function(app, express) {
 
   // parse application/json
   app.use(bodyParser.json());
-}
+};
--- NoteWrangler/server/routes/note.js
+++ NoteWrangler/server/routes/note.js
@@ -1,5 +1,5 @@
 var _ = require('lodash');
-var Note = require('../models/note')
+var Note = require('../models/note');
 
 module.exports = function(app) {
   app.get('/notes', function(req, res) {
@@ -19,7 +19,7 @@ module.exports = function(app) {
     // So things like button isSubmitting states can be demonstrated
     setTimeout(function(){
       res.json(Note.update(req.body));
-    },1000)
+  },1000);
   });
 
   app.get('/notes/:id', function(req, res) {
