4560ca63ca6e99c5e77b72f1ea9002d1224ab6d8
Deyan Yanakiev <kliomba@abv.bg>
2016-04-26 10:54:21 +0300

    CM: Change default route to /customers

--- CustomerManagement/src/components/cm-home/cm-home.js
+++ CustomerManagement/src/components/cm-home/cm-home.js
@@ -1,8 +1,7 @@
 angular.module('custManagement').component('cmHome', {
   templateUrl: 'components/cm-home/cm-home.html',
   $routeConfig: [
-    { path: '/', component: 'cmCustomers', useAsDefault: true },
-    { path: '/customers/', component: 'cmCustomers' },
+    { path: '/customers/', component: 'cmCustomers', useAsDefault: true },
     { path: '/customers/:id', component: 'cmCustomerDetails' },
     { path: '/orders', component: 'cmOrders' }
   ]
