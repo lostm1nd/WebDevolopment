c97b0c0df1b21bcd6d0b263e6363fa1acff82a48
Deyan Yanakiev <kliomba@abv.bg>
2016-04-26 19:31:49 +0300

    CM: Add mongo db

--- CustomerManagement/src/components/cm-customers/cm-customers.js
+++ CustomerManagement/src/components/cm-customers/cm-customers.js
@@ -1,7 +1,7 @@
 angular.module('custManagement').component('cmCustomers', {
   templateUrl: 'components/cm-customers/cm-customers.html',
-  controller: function ($scope, customersDB) {
-    $scope.customers = customersDB;
+  controller: function ($scope, databaseService) {
+    $scope.customers = databaseService.query();
 
     $scope.delete = function (customer) {
       var id = customer.id;
--- CustomerManagement/src/services/database-service.js
+++ CustomerManagement/src/services/database-service.js
@@ -1,3 +1,10 @@
-angular.module('custManagement').factory(function ($resource) {
+angular.module('custManagement').factory('databaseService', function ($resource) {
+  var dbUrl = 'https://api.mlab.com/api/1/databases/customer_management_db/collections/customers/:id';
+  var apiKey = 'vSgKUIIpRUeL8VAEl_WLt9wkrOlDDjNz';
 
+  return $resource(dbUrl, {
+    apiKey: apiKey
+  }, {
+    update: { method: 'PUT' }
+  });
 });
