e0c794f57a9836c4ceb20aecf2dfc5b9c8525478
Deyan Yanakiev <kliomba@abv.bg>
2016-04-26 16:06:51 +0300

    CM: Implement removing a customer

--- CustomerManagement/src/components/cm-customers/cm-customers.js
+++ CustomerManagement/src/components/cm-customers/cm-customers.js
@@ -2,5 +2,14 @@ angular.module('custManagement').component('cmCustomers', {
   templateUrl: 'components/cm-customers/cm-customers.html',
   controller: function ($scope, customersDB) {
     $scope.customers = customersDB;
+
+    $scope.delete = function (customer) {
+      var id = customer.id;
+
+      customersDB = customersDB.filter(function (cust) {
+        return cust.id !== id;
+      });
+      $scope.customers = customersDB;
+    };
   }
 });
