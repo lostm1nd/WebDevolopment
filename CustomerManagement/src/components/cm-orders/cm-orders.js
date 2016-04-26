6d2a1ed785129332918419ed9105b7b1c3a1c774
Deyan Yanakiev <kliomba@abv.bg>
2016-04-26 10:07:11 +0300

    CM: Add total order price and currency filter

--- CustomerManagement/src/components/cm-orders/cm-orders.js
+++ CustomerManagement/src/components/cm-orders/cm-orders.js
@@ -2,5 +2,11 @@ angular.module('custManagement').component('cmOrders', {
   templateUrl: 'components/cm-orders/cm-orders.html',
   controller: function ($scope, customersDB) {
     $scope.customers = customersDB;
+
+    $scope.calculateCustomerTotal = function (customer) {
+      return customer.orders.reduce(function (acc, order) {
+        return acc + order.qty * order.price;
+      }, 0);
+    };
   }
 });
