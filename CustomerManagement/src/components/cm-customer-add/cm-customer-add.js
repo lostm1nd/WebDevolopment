16d4244d457cce49dfdb22da639ad98f0944f592
Deyan Yanakiev <kliomba@abv.bg>
2016-04-26 15:50:57 +0300

    CM: Implement adding a new customer

--- CustomerManagement/src/components/cm-customer-add/cm-customer-add.js
+++ CustomerManagement/src/components/cm-customer-add/cm-customer-add.js
@@ -1,5 +1,17 @@
 angular.module('custManagement').component('cmCustomerAdd', {
   templateUrl: 'components/cm-customer-add/cm-customer-add.html',
-  controller: function () {
+  controller: function ($scope, customersDB) {
+    $scope.customer = {};
+
+    $scope.add = function (customer) {
+      var nextId = customersDB[customersDB.length - 1].id + 1;
+
+      customer.id = nextId;
+      customer.orders = [];
+
+      customersDB.push(customer);
+
+      $scope.customer = {};
+    };
   }
 });
